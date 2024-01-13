55361b47ae6f3a2070e67efc76854a3241c83aa6
AnasGrzor <103470289+AnasGrzor@users.noreply.github.com>
2024-01-13 18:49:54 +0500

    Added Exit function

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import subprocess
 def download_video():
     while True:
       # Prompt the user for the video URL
-      url = input("Enter the YouTube video URL: ")
+      url = input("Enter the YouTube video URL (or type 'exit' to quit): ")
+
+      # Check if the user wants to exit
+      if url.lower() == 'exit':
+        print("Exiting...")
+        break
 
       # Command to download the highest quality video using yt-dlp with the original title as the filename
       command = ['yt-dlp', '-o', '%(title)s.%(ext)s', '-f', 'bestvideo+bestaudio/best', '--merge-output-format', 'mkv', url]
